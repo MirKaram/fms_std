c1ff7e5b23df779cb6e92bcf6a33299d96e5aa09
MirKaram <mirkaramm@ymail.com>
2022-05-03 17:53:45 +0500

    2nd commit

--- app/src/main/java/com/example/secure/feemanagmentsystem/CheckInternet.kt
+++ app/src/main/java/com/example/secure/feemanagmentsystem/CheckInternet.kt
@@ -8,22 +8,8 @@ import android.os.Build
 object CheckInternet {
 
     fun isInternetConnected(getApplicationContext: Context): Boolean {
-        var status = false
         val cm =
             getApplicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (cm != null) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (cm.activeNetwork != null && cm.getNetworkCapabilities(cm.activeNetwork) != null) {
-                    // connected to the internet
-                    status = true
-                }
-            } else {
-                if (cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnectedOrConnecting) {
-                    // connected to the internet
-                    status = true
-                }
-            }
-        }
-        return status
+        return cm.activeNetwork != null && cm.getNetworkCapabilities(cm.activeNetwork) != null
     }
 }
